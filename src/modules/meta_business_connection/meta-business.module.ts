1b0c5ab18cfa53d69dd451e19de8ebbe8491c8ce
rahatalmas <rahat.almas5599@gmail.com>
2026-01-22 22:07:30 +0600

    error fixed: business-controller

--- src/modules/meta_business_connection/meta-business.module.ts
+++ src/modules/meta_business_connection/meta-business.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MetaBusinessController } from './meta-business.controller';
 
 @Module({
-  controllers: [MetaBusinessControllrer],
+  controllers: [MetaBusinessController],
   providers: [],
 })
 export class MetaBusinessModule {}
